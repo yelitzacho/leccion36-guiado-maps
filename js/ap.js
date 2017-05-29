6d063558cc58cef18d8f8de5fe96fd7ea2c0f9af
Yelitza Cho <yelitza.choque@gmail.com>
2017-05-29 10:38:00 -0500

    otro commit

--- js/ap.js
+++ js/ap.js
@@ -11,24 +11,23 @@ function initMap() {
   });
 
   function buscar(){
-  if(navigator.geolocation){
-    navigator.geolocation.getCurrentPosition(funcionExito, funcionError);
-    }
+    if(navigator.geolocation){
+      navigator.geolocation.getCurrentPosition(funcionExito, funcionError);
+      }
   }
 
-  var latitud, longitud;
+  var latitud, longitud, miUbicacion;
   var funcionExito = function(posicion){
     latitud = posicion.coords.latitude;
     longitud = posicion.coords.longitude;
 
-    var miUbicacion = new google.maps.Marker({
+    miUbicacion = new google.maps.Marker({
       position: {lat: latitud, lng: longitud},
       map: map
     });
 
     map.setZoom(18);
     map.setCenter({lat:latitud, lng:longitud});
-
   }
 
   var funcionError = function (error){
@@ -38,4 +37,51 @@ function initMap() {
   var encuentrame = document.getElementById("encuentrame");
   encuentrame.addEventListener("click", buscar);
 
+  var inputPartida = document.getElementById("punto-partida");
+  var inputDestino = document.getElementById("punto-destino");
+
+  new google.maps.places.Autocomplete(inputPartida);
+  new google.maps.places.Autocomplete(inputDestino);
+
+  var directionsService = new google.maps.DirectionsService;
+  var directionsDisplay  = new google.maps.DirectionsRenderer;
+
+  var calculateAndDisplayRoute = function(directionsService, directionsDisplay){
+    directionsService.route({
+      origin: inputPartida.value,
+      destination: inputDestino.value,
+      travelMode: 'DRIVING',
+    }, function(response, status){
+      if (status==='OK'){
+        var distancia = Number((responsive.routes[0].legs[0].distance.text.replace("km","")).replace("","."));
+
+        tarifa.classList.remove("none");
+
+        var costo = distancia*1.75;
+        if (costo<4){
+          tarifa.innerHTML="S/. 4";
+        }
+
+        tarifa.innerHTML = "S/. "+ parseInt(costo);
+
+        directionsDisplay.setDirections(response);
+
+        miUbicacion.setMap(null);
+
+      } else {
+        window.alert("No encontramos una ruta.");
+      }
+
+    });
+
+  }
+
+  directionsDisplay.setMap(map);
+
+  var trazarRuta = function(){
+    calculateAndDisplayRoute(directionsService, directionsDisplay);
+  }
+
+  var ruta = document.getElementById("trazar-ruta");
+  ruta.addEventListener("click", trazarRuta);
 }
